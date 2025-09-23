ab5e2f363e930fe40162c96704dacd9defdcc150
glep-pyto <glep320@gmail.com>
2025-09-23 08:44:31 +0000

    made main edits, waiting for movies to be imported

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -11,5 +12,12 @@ def home():
 @app.route('/select')
 def select():
    return render_template('select_mood.html')  
+@app.route('/results')
+def results():
+   return render_template('results.html', method=['GET','POST'])  
+
+
+
 if __name__ == '__main__':
         app.run(debug=True)
+
