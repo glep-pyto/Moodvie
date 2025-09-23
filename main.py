60861caa8d8e5c600ae9d6cf30df3e08a74255ce
glep-pyto <glep320@gmail.com>
2025-09-23 06:51:07 +0000

    further test, need someone else to run file

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from Flask import Flask, render_template
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -8,6 +8,8 @@ def home():
         title = "Moodvie"
         message = "Welcome to Moodvie, select a mood."
         return render_template('homepage.html', title=title, message=message)
-
+@app.route('/select')
+def select():
+    return render_template('select_mood.html')  
 if __name__ == '__main__':
         app.run(debug=True)
