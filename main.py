554af1fb6b5e0b635c7c5c10e00805cb9ff163b9
glep-pyto <glep320@gmail.com>
2025-09-24 10:23:43 +0000

    added comments for presentation

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
-from movie_data import 
-
+#from movie_data import happy_movies, 
+#import a flask module and database from moviedata file
+#flask is python module used to interact with internet files, mainly html similar to Django
 app = Flask(__name__)
 
 @app.route('/')
@@ -9,10 +10,12 @@ def home():
         title = "Moodvie"
         message = "Welcome to Moodvie, select a mood."
         return render_template('homepage.html', title=title, message=message)
-@app.route('/select')
+@app.route('/select')#when the button is pressed in homepage, it redirects to selections
 def select():
    return render_template('select_mood.html')  
 @app.route('/results', methods=['GET','POST'])
+#GET receives data from the server, in this case from selectmood html
+#POST enables data to be sent over to server
 def results():
    return render_template('results.html')  
 
